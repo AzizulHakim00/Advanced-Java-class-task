125da77b5ac56d3a3413141c5503241035d7b2e2
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-07-30 20:53:12 +0600

    Move FocusForge home controller

---
+++ FocusForge/src/main/java/bd/edu/seu/classproject/controller/HomeController.java
@@ -0,0 +1,13 @@
+package bd.edu.seu.classproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToFocusForge() {
+        return "redirect:/focusforge/dashboard";
+    }
+}
