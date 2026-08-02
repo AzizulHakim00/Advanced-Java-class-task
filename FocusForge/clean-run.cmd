1263dd1a7730bff04d3bc3eac052b7afc6701218
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-08-02 17:09:05 +0600

    Make clean-run remove every legacy package and build artifact

--- FocusForge/clean-run.cmd
+++ FocusForge/clean-run.cmd
@@ -2,15 +2,38 @@
 setlocal
 cd /d "%~dp0"
 
-echo [FocusForge] Removing stale Maven build output...
+echo [FocusForge] Removing stale build output...
 if exist target rmdir /s /q target
+if exist out rmdir /s /q out
 
-echo [FocusForge] Starting a clean Spring Boot build...
-call mvnw.cmd clean spring-boot:run
+echo [FocusForge] Removing legacy local source files from older package layouts...
+for %%F in (
+    "src\main\java\bd\edu\seu\classproject\FocusForgeController.java"
+    "src\main\java\bd\edu\seu\classproject\HomeController.java"
+    "src\main\java\bd\edu\seu\classproject\StudyTask.java"
+    "src\main\java\bd\edu\seu\classproject\StudyCheckIn.java"
+    "src\main\java\bd\edu\seu\classproject\RecommendationResult.java"
+    "src\main\java\bd\edu\seu\classproject\StudyTaskInterface.java"
+) do (
+    if exist %%F del /f /q %%F
+)
+
+for %%D in (
+    "src\main\java\bd\edu\seu\classproject\controller"
+    "src\main\java\bd\edu\seu\classproject\model"
+    "src\main\java\bd\edu\seu\classproject\repository"
+    "src\main\java\bd\edu\seu\classproject\service"
+) do (
+    if exist %%D rmdir /s /q %%D
+)
+
+echo [FocusForge] Running a fresh Maven build and application startup...
+call mvnw.cmd -U clean spring-boot:run
 
 if errorlevel 1 (
     echo.
-    echo [FocusForge] Clean run failed. Check the MySQL server and application.properties credentials.
+    echo [FocusForge] Startup failed.
+    echo Check that MySQL is running and application.properties has the correct username/password.
     pause
     exit /b 1
 )
