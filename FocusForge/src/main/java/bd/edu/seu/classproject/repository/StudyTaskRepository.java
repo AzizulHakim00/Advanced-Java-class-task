235b4f8122408726b3caabda237ef6bd31949bbf
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-08-02 16:41:11 +0600

    Remove old layered repository

--- FocusForge/src/main/java/bd/edu/seu/classproject/repository/StudyTaskRepository.java
+++
@@ -1,7 +0,0 @@
-package bd.edu.seu.classproject.repository;
-
-import bd.edu.seu.classproject.model.StudyTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyTaskRepository extends JpaRepository<StudyTask, Integer> {
-}
